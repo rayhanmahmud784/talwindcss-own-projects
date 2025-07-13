1d95fdfaa62a1461a4a1a5ee30d09880c7bf9ea4
New-Css-Project <raihanmahmud784@gmail.com>
2025-07-13 15:49:51 +0600

    my

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+    darkMode: 'class',
+  content: ['./index.html', './src/**/*.{html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
